1d2927cebceafaf517cca0c100aea28666d67872
Pro-Code-art <jayme.coder@gmail.com>
2021-02-07 17:47:36 -0800

    changed the order the columns are displayed in first query

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
+    password: 'caballodeViento3;',
     database: 'companyManagementToolDB'
 });
 
@@ -41,7 +41,7 @@ const userChoice = () => {
 // Display data based on user choice
 const handleAnswer = (answer) => {
     if (answer.interaction === 'View All Employees') {
-        connection.query('SELECT employees.id, first_name, last_name, title, salary, department FROM employees LEFT JOIN roles ON role_id = department_id LEFT JOIN departments ON departments.id = department_id', (err, res) => {
+        connection.query('SELECT employees.id, first_name, last_name, department, title, salary FROM employees LEFT JOIN roles ON role_id = department_id LEFT JOIN departments ON departments.id = department_id', (err, res) => {
             if (err) throw err;
             console.table(res);
             userChoice();
