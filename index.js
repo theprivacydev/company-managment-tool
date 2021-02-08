73b4c0f2ebb129c64d5b471d3b8985ac1adaa509
Pro-Code-art <jayme.coder@gmail.com>
2021-02-07 19:24:47 -0800

    successfully prompts user for employee info when they select to add employee, but adding to database is unsuccessful

--- index.js
+++ index.js
@@ -7,18 +7,19 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'caballodeViento3;',
+    password: '',
     database: 'companyManagementToolDB'
 });
 
+// Initiate connection and initiate app by calling first inquirer prompts
 connection.connect((err) => {
     if (err) throw err.stack;
     console.log(connection.threadId);
-    userChoice();
+    firstUserChoice();
 });
 
-
-const userChoice = () => {
+// First inquirer prompt
+const firstUserChoice = () => {
     inquirer.prompt(
         {
             name: 'interaction',
@@ -27,7 +28,7 @@ const userChoice = () => {
             choices: [
                 'View All Employees',
                 'View All Employees By Department',
-                'View All Employees Manager',
+                'View All Employees By Manager',
                 'Add Employee',
                 'Remove Employee',
                 'Update Employee Role',
@@ -38,14 +39,70 @@ const userChoice = () => {
         }).then(handleAnswer);
 }
 
+// Get employee info, then add to database
+const employeeInfo = 
+    [
+        {
+            name: 'first',
+            type: 'input',
+            message: 'What is the employee\'s first name?',
+        },
+        {
+            name: 'last',
+            type: 'input',
+            message: 'What is the employee\'s last name?',
+        },
+        {
+            name: 'role',
+            type: 'list',
+            message: 'What is the employee\'s role?',
+            choices: [
+                'Sales Executive',
+                'Engineer',
+                'Accountant',
+                'Data Analyst',
+                'Brand Specialist',
+                'Secretary',
+                'Truck Driver',
+                'IT Tech',
+            ]
+        },
+        {
+            name: 'manager',
+            type: 'list',
+            message: 'Who is the employee\'s Manager?',
+            choices: [
+                'Denise Jenkins',
+                'Darrel Johnson',
+                'Rob Hammond',
+            ]
+        }
+    ];
+
 // Display data based on user choice
 const handleAnswer = (answer) => {
     if (answer.interaction === 'View All Employees') {
-        connection.query('SELECT employees.id, first_name, last_name, department, title, salary FROM employees LEFT JOIN roles ON role_id = department_id LEFT JOIN departments ON departments.id = department_id', (err, res) => {
+        const query1 = 'SELECT employees.id, first_name, last_name, department, title, salary FROM employees LEFT JOIN roles ON role_id = department_id LEFT JOIN departments ON departments.id = department_id';
+        connection.query(query1, (err, res) => {
             if (err) throw err;
             console.table(res);
-            userChoice();
+            firstUserChoice();
           });
-
     }
+
+    if (answer.interaction === 'Add Employee') {
+        inquirer.prompt(employeeInfo).then(addToDatabase)
+    }
+}
+
+
+const addToDatabase = (response) => {
+    const query2 = 'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?), (?), (?), (?)'
+    connection.query(query2, 
+        [response.first, response.last, response.role, response.manager], 
+        (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        firstUserChoice();
+      });
 }
